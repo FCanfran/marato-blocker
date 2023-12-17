90f2555aae41ece3ed1a24c082192c979340dea2
FCanfran <ferchomartin9@gmail.com>
2023-12-17 11:47:48 +0100

    upload final version

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.webNavigation.onCompleted.addListener(redirectIfNecessary);
-  });
+const blockList = ["https://es.pornhub.com/", "https://www.pornhub.com/"];
+    
+  chrome.runtime.onInstalled.addListener(function() {
+        chrome.webNavigation.onCompleted.addListener(redirectIfNecessary);
+    });
   
   function redirectIfNecessary(details) {
     // Check if the URL matches the desired page
-    if (details.url.includes("https://es.pornhub.com/")) {
+    if (blockList.find(blocked => details.url.includes(blocked)) != undefined) {
       // Redirect to another page
       chrome.tabs.update(details.tabId, {url: "https://fcanfran.github.io/marato-blocker/"});
     }
